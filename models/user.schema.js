d59af8190130783fa996aff89e41120285aec71a
Danesh Tolani <danesh.tolani@gmail.com>
2023-01-07 12:18:51 +0530

    modified user model

--- models/user.schema.js
+++ models/user.schema.js
@@ -1,9 +1,33 @@
 import mongoose from "mongoose";
+import AuthRoles from "../utils/authRoles";
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Name is a required"],
-    maxLength: [50, "Name must be less than 50 characters"],
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is a required"],
+      maxLength: [50, "Name must be less than 50 characters"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is a required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is a required"],
+      minLength: [8, "Password must be at least 8 characters"],
+      select: false, // when we query the database while authentication, this field will never come up
+    },
+    role: {
+      type: String,
+      enum: Object.values(AuthRoles), // returns array of values ["ADMIN", "MODERATOR", "USER"]
+      default: AuthRoles.USER,
+    },
+    forgotPasswordToken: String,
+    forgotPasswordExpiry: Date,
   },
-});
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema); // will be stored as "users" in mongodb
