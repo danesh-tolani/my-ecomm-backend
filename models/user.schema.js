5b022aba16bf912bbc4e47d7feda3c3fd30695a6
Danesh Tolani <danesh.tolani@gmail.com>
2023-01-07 12:57:00 +0530

    hook for userSchema

--- models/user.schema.js
+++ models/user.schema.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 import AuthRoles from "../utils/authRoles";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
 
 const userSchema = new mongoose.Schema(
   {
@@ -29,5 +32,15 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+// challenge 1: encrypt password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.modified("password")) return next();
+  // this checks if the password is there or not, if modified is true than it means password was already there and this encrypt password should not run
+
+  // used normal function because we need to use .this which refers to the userSchema object
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 
 export default mongoose.model("User", userSchema); // will be stored as "users" in mongodb
