dd34b8b7a971ac023326532aff88782644ffe5b1
Danesh Tolani <danesh.tolani@gmail.com>
2023-01-08 12:12:54 +0530

    added asyncHandler and Custom error handler

---
+++ service/asyncHandler.js
@@ -0,0 +1,13 @@
+// Big Promise
+
+// pass a regular function and handle it in a asynschronous way
+const asyncHandler = (fn) => async (req, res, next) => {
+  try {
+    await fn(req, res, next);
+  } catch (error) {
+    res.status(error.code || 500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
