1e2524d1a68566eddcbca6db1dd0fa044aabfe3f
Danesh Tolani <danesh.tolani@gmail.com>
2023-01-11 16:18:27 +0530

    handled imgs with formidable in product controller

---
+++ controllers/productController.js
@@ -0,0 +1,84 @@
+import Product from "../models/product.schema";
+import formidable from "formidable";
+import fs from "fs"; // node filesystem module
+import { s3FileUpload, s3FileDelete } from "../service/imageUploader";
+import Mongoose from "mongoose";
+import asyncHandler from "../service/asyncHandler";
+import CustomError from "../utils/customError";
+import config from "../config/index";
+
+/***************************************************************
+@ADD_PRODUCT
+@route http://localhost:4000/api/product
+@description Controller used for creating a new product 
+@description Only admin can create the coupon
+@description Uses AWS S3 bucket for image upload 
+@return Product Object
+***************************************************************/
+export const addProduct = asyncHandler(async (req, res) => {
+  const form = formidable({
+    multiples: true,
+    keepExtensions: true,
+  });
+
+  form.parse(req, async function (err, fields, files) {
+    try {
+      if (err) {
+        throw new CustomError(err.message || "something went wrong", 500);
+      }
+
+      // generate _id
+      let productId = new Mongoose.Types.ObjectId().toHexString(); // gives a 24 character hex string
+      // we are generating product id because we want to store photos in a certain way
+      // 345ab/photo_1
+      // 345ab/photo_2
+      //   console.log(fields, files);
+
+      // check for fields
+      if (!fields.name || !fields.price || !fields.description || !fields.collectionId) {
+        throw new CustomError(err.message || "something went wrong", 500);
+      }
+
+      // handling images
+      let imgArrayResp = Promise.all(
+        Object.keys(files).map(async (filekey, index) => {
+          const element = files[filekey];
+
+          // reference of the file that is stored in out system
+          const data = fs.readFileSync(element.filepath);
+
+          // upload
+          const upload = await s3FileUpload({
+            bucketName: config.S3_BUCKET_NAME,
+            body: data,
+            contentType: element.mimetype,
+            key: `products/${productId}/photo_${index + 1}.png`,
+          });
+          return { secure_url: upload.Location }; // this location is coming from the promise
+        })
+      );
+
+      let imgArray = await imgArrayResp;
+
+      const product = await Product.create({
+        _id: productId,
+        photos: imgArray,
+        ...fields,
+      });
+
+      if (!product) {
+        throw new CustomError("Product was not created", 500);
+      }
+
+      res.status(200).json({
+        success: true,
+        product,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        message: error.message || "Something went wrong",
+      });
+    }
+  });
+});
